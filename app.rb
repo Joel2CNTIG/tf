5143692542b7664baa297a52a7338d43e3e98b76
Joel <joel.fagersand@elev.ga.ntig.se>
2024-01-11 13:00:45 +0100

    added login and register

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+enable :sessions 
+
 get('/') do
     slim(:"accounts/register", layout: :login_layout)
 end
@@ -12,8 +14,49 @@ get('/login') do
     slim(:"accounts/login", layout: :login_layout)
 end
 
-post('/post_register') do
+post('/post-login') do
+    username = params[:username]
+    password = params[:pwd]
+    db = SQLite3::Database.new("db/user_info.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT password FROM user WHERE username = ?",username).first
+    if result != nil && BCrypt::Password.new(result["password"]) == password
+      session[:username] = username
+      session[:password] = password
+      redirect('/home')
+      if username == "admin" 
+        session[:tag] = "admin"
+      else
+        session[:tag] = "user"
+      end
+    else
+      redirect('/wrong_username_or_pwd')
+    end
+end
+  
+post('/post-register') do
+    username = params[:username]
+    password = params[:pwd]
+    db = SQLite3::Database.new("db/user_info.db")
+    db.results_as_hash = true
+    compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
+    password_digest = BCrypt::Password.create(password)
+    if compared_username == nil
+      db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
+      session[:username] = username
+      session[:password] = password
+      if username == "admin" 
+        session[:tag] = "admin"
+      else
+        session[:tag] = "user"
+      end
+      redirect('/home')
+    else
+      redirect('/username_already_exists')
+    end
 end
 
-post('/post_login') do
+post('/post-guest') do
+    session[:username] = "guest"
+    session[:tag] = "guest"
 end
