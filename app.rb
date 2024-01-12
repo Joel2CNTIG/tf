114292d777d48088f549e91178b3bca9320952da
Joel <joel.fagersand@elev.ga.ntig.se>
2024-01-12 13:09:43 +0100

    lite ändringar till login och register

--- app.rb
+++ app.rb
@@ -15,48 +15,51 @@ get('/login') do
 end
 
 post('/post-login') do
-    username = params[:username]
-    password = params[:pwd]
-    db = SQLite3::Database.new("db/user_info.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT password FROM user WHERE username = ?",username).first
-    if result != nil && BCrypt::Password.new(result["password"]) == password
-      session[:username] = username
-      session[:password] = password
-      redirect('/home')
-      if username == "admin" 
-        session[:tag] = "admin"
-      else
-        session[:tag] = "user"
-      end
+  username = params[:username]
+  password = params[:pwd]
+  db = SQLite3::Database.new("db/user_info.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT password FROM user WHERE username = ?",username).first
+  if result != nil && BCrypt::Password.new(result["password"]) == password
+    session[:username] = username
+    session[:password] = password
+    redirect('/home')
+    if username == "admin" 
+      session[:tag] = "admin"
     else
-      redirect('/wrong_username_or_pwd')
+      session[:tag] = "user"
     end
+    session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+  else
+    redirect('/wrong_username_or_pwd')
+  end
 end
   
 post('/post-register') do
-    username = params[:username]
-    password = params[:pwd]
-    db = SQLite3::Database.new("db/user_info.db")
-    db.results_as_hash = true
-    compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
-    password_digest = BCrypt::Password.create(password)
-    if compared_username == nil
-      db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
-      session[:username] = username
-      session[:password] = password
-      if username == "admin" 
-        session[:tag] = "admin"
-      else
-        session[:tag] = "user"
-      end
-      redirect('/home')
+  username = params[:username]
+  password = params[:pwd]
+  db = SQLite3::Database.new("db/user_info.db")
+  db.results_as_hash = true
+  compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
+  password_digest = BCrypt::Password.create(password)
+  if compared_username == nil
+    db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
+    session[:username] = username
+    session[:password] = password
+    if username == "admin" 
+      session[:tag] = "admin"
     else
-      redirect('/username_already_exists')
+      session[:tag] = "user"
     end
+    session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+    redirect('/home')
+  else
+    redirect('/username_already_exists')
+  end
 end
 
 post('/post-guest') do
-    session[:username] = "guest"
-    session[:tag] = "guest"
+  session[:username] = "guest"
+  session[:tag] = "guest"
+  redirect('/home')
 end
