94f765a18a74b99898e4f24e40c97ae5899e7a81
Joel <joel.fagersand@elev.ga.ntig.se>
2024-01-24 09:07:08 +0100

    ändringar och tillägg till lite allt möjligt

--- app.rb
+++ app.rb
@@ -6,30 +6,34 @@ require 'bcrypt'
 
 enable :sessions 
 
+def connect_db(path)
+  return SQLite3::Database.new(path)
+end
+
 get('/') do
-    slim(:"accounts/register", layout: :login_layout)
+  slim(:"accounts/register", layout: :login_layout)
 end
 
 get('/login') do
-    slim(:"accounts/login", layout: :login_layout)
+  slim(:"accounts/login", layout: :login_layout)
 end
 
 post('/post-login') do
   username = params[:username]
   password = params[:pwd]
-  db = SQLite3::Database.new("db/user_info.db")
+  db = connect_db("db/user_info.db")
   db.results_as_hash = true
   result = db.execute("SELECT password FROM user WHERE username = ?",username).first
   if result != nil && BCrypt::Password.new(result["password"]) == password
     session[:username] = username
     session[:password] = password
-    redirect('/home')
     if username == "admin" 
       session[:tag] = "admin"
     else
       session[:tag] = "user"
     end
     session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+    redirect('/home')
   else
     redirect('/wrong_username_or_pwd')
   end
@@ -38,11 +42,11 @@ end
 post('/post-register') do
   username = params[:username]
   password = params[:pwd]
-  db = SQLite3::Database.new("db/user_info.db")
+  db = connect_db("db/user_info.db")
   db.results_as_hash = true
   compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
   password_digest = BCrypt::Password.create(password)
-  if compared_username == nil
+  if compared_username == nil && username.length < 20
     db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
     session[:username] = username
     session[:password] = password
@@ -62,4 +66,27 @@ post('/post-guest') do
   session[:username] = "guest"
   session[:tag] = "guest"
   redirect('/home')
+end
+
+get('/home') do
+  @db = connect_db("db/user_info.db")
+  @db.results_as_hash = true
+  @recent = @db.execute("SELECT title FROM projects ORDER BY id DESC LIMIT 5")
+  p @result
+  slim(:"site/home")
+end
+
+get('/account/:id') do
+  @id = params[:id]
+  @db = connect_db("db/user_info.db")
+  @db.results_as_hash = true
+  @result = @db.execute("SELECT title FROM projects WHERE user_id = ?", @id)
+  slim(:"site/account")
+end
+
+get('/create') do
+  db = connect_db("db/user_info.db")
+  db.results_as_hash = true
+  @result = db.execute("SELECT word FROM keywords")
+  slim(:"site/create")
 end
