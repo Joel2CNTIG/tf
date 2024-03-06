5d4b90896137a262ac26cd16ae6b8aa41ccc3c20
Joel <joel.fagersand@elev.ga.ntig.se>
2024-03-06 10:17:04 +0100

    flyttat kod till separat dokument samt mindre ändringar och tillägg

--- backup_code_in_case_shit_goes_terribly_wrong.rb
+++ backup_code_in_case_shit_goes_terribly_wrong.rb
@@ -3,14 +3,34 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
-require_relative 'functions.rb'
-enable :sessions 
 
+enable :sessions 
 
 before do
   db = connect_db("db/user_info.db")
   db.results_as_hash = true
-  before(db)
+  restricted_routes = ['/account/:id', '/create']
+  login_routes = ['/', '/login', '/post-login', '/post-register', '/post-guest', '/wrong_username_or_pwd', '/username_too_long', '/username_already_exists', '/post-too_long']
+  if !login_routes.include?(request.path_info) && session[:tag] != "guest" && session[:username] != db.execute("SELECT username FROM user WHERE id = ?", session[:id]).first["username"]
+    redirect('/')
+    session[:tag] = nil
+    session[:username] = nil
+    session[:password] = nil
+    session[:status] = nil 
+  end
+  if session[:tag] == "guest" && restricted_routes.include?(request.path_info)
+    redirect('/home')
+  end
+  if session[:tag] != "admin" && request.path_info == '/admin/*' 
+    redirect('/home')
+  end
+  if session[:tag] == nil && !login_routes.include?(request.path_info)
+    redirect('/')
+  end
+end
+
+def connect_db(path)
+  return SQLite3::Database.new(path)
 end
 
 get('/') do
@@ -27,7 +47,20 @@ post('/post-login') do
   db = connect_db("db/user_info.db")
   db.results_as_hash = true
   result = db.execute("SELECT password FROM user WHERE username = ?",username).first
-  post_login(db, username, password)
+  if result != nil && BCrypt::Password.new(result["password"]) == password
+    session[:username] = username
+    session[:password] = password
+    if username == "admin" 
+      session[:tag] = "admin"
+    else
+      session[:tag] = "user"
+    end
+    session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+    redirect('/home')
+  else
+    session[:status] = "wrong_user_or_pwd"
+    redirect('/login')
+  end
 end
   
 post('/post-register') do
@@ -36,7 +69,40 @@ post('/post-register') do
   password_again = params[:pwd_again]
   db = connect_db("db/user_info.db")
   db.results_as_hash = true
-  post_register(db, username, password, password_again)
+  compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
+  password_digest = BCrypt::Password.create(password)
+  if username.length > 20
+    session[:status] = "toolong"
+    redirect('/')
+  end
+  forbidden_chars = [" ", ",", ":", ";", "?", "!", "]", "[", "&", "=", "}", "{", "%", "¤", "$", "#", "£", "'", "@", "ä", "å", "ö", "|", "<", ">"]
+  forbidden_chars.each do |char|
+    if username.include?(char)
+      session[:status] = "forbiddenchar"
+      redirect('/')
+    end
+  end
+  if password_again == password
+    if compared_username == nil 
+      db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
+      session[:username] = username
+      session[:password] = password
+      session[:status] = nil
+      if username == "admin" 
+        session[:tag] = "admin"
+      else
+        session[:tag] = "user"
+      end
+      session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+      redirect('/home')
+    else
+      session[:status] = "alreadyexists"
+      redirect('/')
+    end
+  else
+    session[:status] = "nomatch"
+    redirect('/')
+  end
 end
 
 post('/post-guest') do
@@ -50,6 +116,7 @@ get('/home') do
   @db.results_as_hash = true
   @recent = @db.execute("SELECT title FROM projects ORDER BY id DESC LIMIT 5")
   @popular = @db.execute("SELECT title FROM projects ORDER By visits DESC LIMIT 5")
+  p @recent
   slim(:"site/home")
 end
 
@@ -76,10 +143,23 @@ post('/post-create') do
   title = params[:title]
   description = params[:description]
   price = params[:price]
-  keyword1 = params[:keyword]
-  keyword2 = params[:keyword2]
-  keyword3 = params[:keyword3]
-  post_create(db, id, title, description, price, keyword1, keyword2, keyword3)
+  keywords = []
+  unless params[:keyword] == nil || params[:keyword] == ""
+    keywords << params[:keyword]
+  end
+  unless params[:keyword2] == nil || params[:keyword2] == ""
+    keywords << params[:keyword2]
+  end
+  unless params[:keyword3] == nil || params[:keyword3] == ""
+    keywords << params[:keyword3]
+  end
+  db.execute("INSERT INTO projects (user_id, title, description, visits, price) VALUES (?,?,?,?,?)", id, title, description, 0, price)
+  proj_id = db.execute("SELECT id FROM projects WHERE title = ?", title).first["id"]
+  keywords.each do |word|
+    key_id = db.execute("SELECT id FROM keywords WHERE word = ?", word).first["id"]
+    db.execute("INSERT INTO project_keyword_relationship (project_id, keyword_id) VALUES (?,?)", proj_id, key_id)
+  end
+  redirect('/home')
 end
 
 get('/project/:id') do
@@ -129,10 +209,30 @@ post('/project/:id/post-edit') do
   title = params[:title]
   description = params[:description]
   price = params[:price]
-  keyword1 = params[:keyword]
-  keyword2 = params[:keyword2]
-  keyword3 = params[:keyword3]
-  post_edit(db, id, title, description, price, keyword1, keyword2, keyword3)
+  keywords = []
+  unless params[:keyword] == nil || params[:keyword] == ""
+    keywords << params[:keyword]
+  end
+  unless params[:keyword2] == nil || params[:keyword2] == ""
+    keywords << params[:keyword2]
+  end
+  unless params[:keyword3] == nil || params[:keyword3] == ""
+    keywords << params[:keyword3]
+  end
+  unless title == ""
+    db.execute("UPDATE projects SET title = ? WHERE id = #{params[:id]}", title)
+  end
+  unless description == ""
+    db.execute("UPDATE projects SET description = ? WHERE id = #{params[:id]}", description)
+  end
+  unless price == ""
+    db.execute("UPDATE projects SET price = ? WHERE id = #{params[:id]}", price)
+  end
+  keywords.each do |word|
+    key_id = db.execute("SELECT id FROM keywords WHERE word = ?", word).first["id"]
+    db.execute("INSERT INTO project_keyword_relationship (project_id, keyword_id) VALUES (?,?)", params[:id], key_id)
+  end
+  redirect("/project/#{params[:id]}")
 end
 
 before('/project/:id/delete') do
@@ -236,14 +336,38 @@ post('/settings/:id/post-delete_account') do
   db.results_as_hash = true
   username = params[:username]
   password = params[:pwd]
-  post_settings_delete_account(db, id, username, password)
+  compared_username = db.execute("SELECT username FROM user WHERE id = ?", id).first["username"]
+  compared_password = db.execute("SELECT password FROM user WHERE id = ?", id).first["password"]
+  if username == compared_username && BCrypt::Password.new(compared_password) == password && session[:tag] != "admin"
+    project_ids = db.execute("SELECT id FROM projects WHERE user_id = ?", id)
+    project_ids.each do |projid|
+      id = projid["id"]
+      db.execute("DELETE FROM projects WHERE id = ?", id)
+      db.execute("DELETE FROM project_keyword_relationship WHERE project_id = ?", id)
+    end
+    db.execute("DELETE FROM user WHERE id = ?", id)
+    session[:username] = nil
+    session[:password] = nil
+    session[:tag] = nil
+    session[:status] = nil
+    redirect('/')
+  else
+    redirect("settings/#{id}/delete_account")
+  end
 end
 
 post('/admin/:id/post-delete_account') do
   id = params[:id]
   db = connect_db("db/user_info.db")
   db.results_as_hash = true
-  post_admin_delete_account(db, id)
+  project_ids = db.execute("SELECT id FROM projects WHERE user_id = ?", id)
+  project_ids.each do |projid|
+    project_id = projid["id"]
+    db.execute("DELETE FROM project_keyword_relationship WHERE project_id = ?", project_id)
+  end
+  db.execute("DELETE FROM projects WHERE user_id = ?", id)
+  db.execute("DELETE FROM user WHERE id = ?", id)
+  redirect('/../admin/manage_accounts')
 end
 
 get('/home/search/:key1/:key2/:key3') do
@@ -251,7 +375,18 @@ get('/home/search/:key1/:key2/:key3') do
   @db.results_as_hash = true
   @result = @db.execute("SELECT word FROM keywords")
   keyids = [params[:key1], params[:key2], params[:key3]]
-  @projects = keyid_array_to_title_array(keyids)
+  p keyids
+  p @db.execute("SELECT id FROM keywords WHERE word = ?", "cooking").first["id"]
+  project_ids = []
+  keyids.each do |id|
+    project_ids << @db.execute("SELECT project_id FROM project_keyword_relationship WHERE keyword_id = ?", id) 
+  end
+  @projects = []
+  project_ids.each do |idarray|
+    idarray.each do |id|
+      @projects << @db.execute("SELECT title FROM projects WHERE id = ?", id["project_id"]).first
+    end
+  end
   if keyids == ["none", "none", "none"]
     @projects = @db.execute("SELECt title FROM projects")
   end
@@ -262,10 +397,22 @@ end
 post('/post-search') do
   db = connect_db("db/user_info.db")
   db.results_as_hash = true
-  keyword1 = params[:keyword]
-  keyword2 = params[:keyword2]
-  keyword3 = params[:keyword3]
-  post_search(db, keyword1, keyword2, keyword3)
+  if params[:keyword] == ""
+    key1 = "none"
+  else
+    key1 = db.execute("SELECT id FROM keywords WHERE word = ?", params[:keyword]).first["id"]
+  end
+  if params[:keyword2] == ""
+    key2 = "none"
+  else
+    key2 = db.execute("SELECT id FROM keywords WHERE word = ?", params[:keyword2]).first["id"]
+  end
+  if params[:keyword3] == ""
+    key3 = "none"
+  else
+    key3 = db.execute("SELECT id FROM keywords WHERE word = ?", params[:keyword3]).first["id"]
+  end
+  redirect("/home/search/#{key1}/#{key2}/#{key3}")
 end
 
 post('/post-logout') do
---
+++ functions.rb
@@ -0,0 +1,234 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions 
+
+
+def average(arr)
+  return arr.sum/arr.size
+end
+
+def timeout(time_arr)
+  time_arr = time_arr.last[5]
+  time_intervals = []
+  compared_time = time_arr[0]
+  time_arr.each do |time|
+    new_time = time - compared_time
+    time_intervals << new_time
+  end
+  if average(time_intervals) <= 1
+    session[:status] = "toofast"
+    redirect('/')
+  end
+end
+
+def connect_db(path)
+  return SQLite3::Database.new(path)
+end
+
+def before(db)
+  restricted_routes = ['/account/:id', '/create']
+  login_routes = ['/', '/login', '/post-login', '/post-register', '/post-guest', '/wrong_username_or_pwd', '/username_too_long', '/username_already_exists', '/post-too_long']
+  if !login_routes.include?(request.path_info) && session[:tag] != "guest" && session[:username] != db.execute("SELECT username FROM user WHERE id = ?", session[:id]).first["username"]
+    redirect('/')
+    session[:tag] = nil
+    session[:username] = nil
+    session[:password] = nil
+    session[:status] = nil 
+  end
+  if session[:tag] == "guest" && restricted_routes.include?(request.path_info)
+    redirect('/home')
+  end
+  if session[:tag] != "admin" && request.path_info == '/admin/*' 
+    redirect('/home')
+  end
+  if session[:tag] == nil && !login_routes.include?(request.path_info)
+    redirect('/')
+  end
+end
+
+def post_login(db, username, password)
+  result = db.execute("SELECT password FROM user WHERE username = ?",username).first
+  if result != nil && BCrypt::Password.new(result["password"]) == password
+    session[:username] = username
+    session[:password] = password
+    if username == "admin" 
+      session[:tag] = "admin"
+    else
+      session[:tag] = "user"
+    end
+    session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+    redirect('/home')
+  else
+    session[:status] = "wrong_user_or_pwd"
+    redirect('/login')
+  end
+end
+  
+def post_register(db, username, password, password_again)
+  compared_username = db.execute("SELECT username FROM user WHERE username LIKE ?",username).first
+  password_digest = BCrypt::Password.create(password)
+  if username.length > 20
+    session[:status] = "toolong"
+    redirect('/')
+  end
+  forbidden_chars = [" ", ",", ":", ";", "?", "!", "]", "[", "&", "=", "}", "{", "%", "¤", "$", "#", "£", "'", "@", "ä", "å", "ö", "|", "<", ">"]
+  forbidden_chars.each do |char|
+    if username.include?(char)
+      session[:status] = "forbiddenchar"
+      redirect('/')
+    end
+  end
+  if password_again == password
+    if compared_username == nil 
+      db.execute("INSERT INTO user (username, password) VALUES (?,?)",username, password_digest)
+      session[:username] = username
+      session[:password] = password
+      session[:status] = nil
+      if username == "admin" 
+        session[:tag] = "admin"
+      else
+        session[:tag] = "user"
+      end
+      session[:id] = db.execute("SELECT id FROM user WHERE username = ?",username).first["id"]
+      redirect('/home')
+    else
+      session[:status] = "alreadyexists"
+      redirect('/')
+    end
+  else
+    session[:status] = "nomatch"
+    redirect('/')
+  end
+end
+
+def post_create(db, id, title, description, price, keyword, keyword2, keyword3)
+  if title == "" || description == "" || price == ""
+    session[:status] = "create_error"
+    redirect('/create')
+  end
+  keywords = []
+  unless keyword == nil || keyword == ""
+    keywords << keyword
+  end
+  unless keyword2 == nil || keyword2 == ""
+    keywords << keyword2
+  end
+  unless keyword3 == nil || keyword3 == ""
+    keywords << keyword3
+  end
+  db.execute("INSERT INTO projects (user_id, title, description, visits, price) VALUES (?,?,?,?,?)", id, title, description, 0, price)
+  proj_id = db.execute("SELECT id FROM projects WHERE title = ?", title).first["id"]
+  keywords.each do |word|
+    key_id = db.execute("SELECT id FROM keywords WHERE word = ?", word).first["id"]
+    db.execute("INSERT INTO project_keyword_relationship (project_id, keyword_id) VALUES (?,?)", proj_id, key_id)
+  end
+  session[:status] = nil
+  redirect('/home')
+end
+
+def post_edit(db, id, title, description, price, keyword1, keyword2, keyword3)
+  keywords = []
+  unless keyword1 == nil || keyword1 == ""
+    keywords << keyword1
+  end
+  unless keyword2 == nil || keyword2 == ""
+    keywords << keyword2
+  end
+  unless keyword3 == nil || keyword3 == ""
+    keywords << keyword3
+  end
+  unless title == ""
+    db.execute("UPDATE projects SET title = ? WHERE id = #{id}", title)
+  end
+  unless description == ""
+    db.execute("UPDATE projects SET description = ? WHERE id = #{id}", description)
+  end
+  unless price == ""
+    db.execute("UPDATE projects SET price = ? WHERE id = #{id}", price)
+  end
+  keywords.each do |word|
+    key_id = db.execute("SELECT id FROM keywords WHERE word = ?", word).first["id"]
+    db.execute("INSERT INTO project_keyword_relationship (project_id, keyword_id) VALUES (?,?)", id, key_id)
+  end
+  redirect("/project/#{id}")
+end
+
+def post_settings_delete_account(db, id, username, password)
+  compared_username = db.execute("SELECT username FROM user WHERE id = ?", id).first["username"]
+  compared_password = db.execute("SELECT password FROM user WHERE id = ?", id).first["password"]
+  if username == compared_username && BCrypt::Password.new(compared_password) == password && session[:tag] != "admin"
+    project_ids = db.execute("SELECT id FROM projects WHERE user_id = ?", id)
+    project_ids.each do |projid|
+      new_project_id = projid["id"]
+      db.execute("DELETE FROM projects WHERE id = ?", new_project_id)
+      db.execute("DELETE FROM project_keyword_relationship WHERE project_id = ?", new_project_id)
+    end
+    db.execute("DELETE FROM user WHERE id = ?", id)
+    session[:username] = nil
+    session[:password] = nil
+    session[:tag] = nil
+    session[:status] = nil
+    redirect('/')
+  else
+    redirect("settings/#{id}/delete_account")
+  end
+end
+
+def post_admin_delete_account(db, id)
+  id = params[:id]
+  db = connect_db("db/user_info.db")
+  db.results_as_hash = true
+  project_ids = db.execute("SELECT id FROM projects WHERE user_id = ?", id)
+  project_ids.each do |projid|
+    project_id = projid["id"]
+    db.execute("DELETE FROM project_keyword_relationship WHERE project_id = ?", project_id)
+  end
+  db.execute("DELETE FROM projects WHERE user_id = ?", id)
+  db.execute("DELETE FROM user WHERE id = ?", id)
+  redirect('/../admin/manage_accounts')
+end
+
+def keyid_array_to_title_array(arr)
+  projid_array = []
+  arr.each do |id|
+    projid_array << @db.execute("SELECT project_id FROM project_keyword_relationship WHERE keyword_id = ?", id) 
+  end
+  titles = []
+  projid_array.each do |idarray|
+    idarray.each do |id|
+      titles << @db.execute("SELECT title FROM projects WHERE id = ?", id["project_id"]).first
+    end
+  end
+  return titles
+end
+
+def post_search(db, keyword1, keyword2, keyword3)
+  if keyword1 == ""
+    key1 = "none"
+  else
+    key1 = db.execute("SELECT id FROM keywords WHERE word = ?", keyword1).first["id"]
+  end
+  if keyword2 == ""
+    key2 = "none"
+  else
+    key2 = db.execute("SELECT id FROM keywords WHERE word = ?", keyword2).first["id"]
+  end
+  if keyword3 == ""
+    key3 = "none"
+  else
+    key3 = db.execute("SELECT id FROM keywords WHERE word = ?", keyword3).first["id"]
+  end
+  redirect("/home/search/#{key1}/#{key2}/#{key3}")
+end
+
+post('/post-logout') do
+  session[:username] = nil
+  session[:tag] = nil
+  session[:password] = nil
+  session[:status] = nil
+  redirect('/')
+end
